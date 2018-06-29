69121ceb08d7374854d05733c147cc3368d0e4dd
David Alejandro Yturralde Maldonado <davidyturralde@hotmail.com>
2018-06-29 14:14:22 -0500

    tarea de sqlite para programacion movil

---
+++ app/src/main/java/facci/pm/ta3/sqlite/trabajoautonomo3sqlite/database/entities/ShoppingItemDB.java
@@ -0,0 +1,149 @@
+package facci.pm.ta3.sqlite.trabajoautonomo3sqlite.database.entities;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.provider.BaseColumns;
+import facci.pm.ta3.sqlite.trabajoautonomo3sqlite.database.helper.ShoppingElementHelper;
+import facci.pm.ta3.sqlite.trabajoautonomo3sqlite.database.model.ShoppingItem;
+import java.util.ArrayList;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ShoppingItemDB {
+
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String COMMA_SEP = ",";
+
+    private ShoppingElementHelper dbHelper;
+
+    public ShoppingItemDB(Context context) {
+        // Create new helper
+        dbHelper = new ShoppingElementHelper(context);
+    }
+
+    /* Inner class that defines the table contents */
+    public static abstract class ShoppingElementEntry implements BaseColumns {
+        public static final String TABLE_NAME = "entry";
+        public static final String COLUMN_NAME_TITLE = "title";
+
+        public static final String CREATE_TABLE = "CREATE TABLE " +
+                TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY AUTOINCREMENT" + COMMA_SEP +
+                COLUMN_NAME_TITLE + TEXT_TYPE + " )";
+
+        public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+
+
+    public void insertElement(String productName) {
+        //TODO: Todo el código necesario para INSERTAR un Item a la Base de datos
+
+        //Partimos por obtener el repositorio de datos en modo de escritura, esto nos permite trabajar
+        //con el mismo ( esto se va a hacer en cada una de las secciones de código a trabajar)
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        //Creamos un nuevo mapa de valores, en el cual vamos por medio de un .put agregarle los nombres
+        //de los productos que va a mostrar nuestra lista
+        ContentValues values = new ContentValues();
+        values.put(ShoppingElementEntry.COLUMN_NAME_TITLE, productName);
+
+        //Por último, gracias al método .insert agregamos estos elementosa nuestra tabla
+        long InsertRows = db.insert(ShoppingElementEntry.TABLE_NAME, null, values);
+
+
+    }
+
+
+    public ArrayList<ShoppingItem> getAllItems() {
+
+        ArrayList<ShoppingItem> shoppingItems = new ArrayList<>();
+
+        String[] allColumns = { ShoppingElementEntry._ID,
+            ShoppingElementEntry.COLUMN_NAME_TITLE};
+
+        Cursor cursor = dbHelper.getReadableDatabase().query(
+            ShoppingElementEntry.TABLE_NAME,    // The table to query
+            allColumns,                         // The columns to return
+            null,                               // The columns for the WHERE clause
+            null,                               // The values for the WHERE clause
+            null,                               // don't group the rows
+            null,                               // don't filter by row groups
+            null                                // The sort order
+        );
+
+        cursor.moveToFirst();
+
+        while (!cursor.isAfterLast()) {
+            ShoppingItem shoppingItem = new ShoppingItem(getItemId(cursor), getItemName(cursor));
+            shoppingItems.add(shoppingItem);
+            cursor.moveToNext();
+        }
+        // make sure to close the cursor
+        cursor.close();
+        dbHelper.getReadableDatabase().close();
+        return shoppingItems;
+    }
+
+    private long getItemId(Cursor cursor) {
+        return cursor.getLong(cursor.getColumnIndexOrThrow(ShoppingElementEntry._ID));
+    }
+
+    private String getItemName(Cursor cursor) {
+        return cursor.getString(cursor.getColumnIndexOrThrow(ShoppingElementEntry.COLUMN_NAME_TITLE));
+    }
+
+
+    public void clearAllItems() {
+        //TODO: Todo el código necesario para ELIMINAR todos los Items de la Base de datos
+
+        /*Estas 2 lineas de código nos sirve para evitar que los datos se dupliquen cada vez que se
+          vuelve a abrir el programa, y asi lograr que la lista contenga solo la cantidad de elementos
+          que definimos al principio
+        */
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        //Por medio del método .delete eliminamos esta tabla al asignar valores null
+        int ClearRows = db.delete(ShoppingElementEntry.TABLE_NAME,null,null);
+
+    }
+
+    public void updateItem(ShoppingItem shoppingItem) {
+        //TODO: Todo el código necesario para ACTUALIZAR un Item en la Base de datos
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String title = shoppingItem.getName();
+        ContentValues values = new ContentValues();
+
+        //En esta linea asignamos los titulos que van a ser ingresados
+        values.put(ShoppingElementEntry.COLUMN_NAME_TITLE,title);
+        //Aqui se selecciona la ID en la cual va a ser actualizados los productos
+        String selection = ShoppingElementEntry._ID + " = ?";
+        //Se obtiene la ID de estos elementos para saber su posición
+        String[] selectionArgs = {String.valueOf(shoppingItem.getId())};
+        //Y por último se usa el método .update para actualizar la lista
+        int updateRows = db.update(ShoppingElementEntry.TABLE_NAME,
+                values,
+                selection,
+                selectionArgs);
+
+    }
+
+    public void deleteItem(ShoppingItem shoppingItem) {
+        //TODO: Todo el código necesario para ELIMINAR un Item de la Base de datos
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        //A diferencia del .delete en el cual borramos toda la lista, en este solo borramos
+        //las tablas que deseamos y previamente fueron asignadas en el MainActivity, esto
+        //se logra gracias al métoo .delete
+        String selection = ShoppingElementEntry.COLUMN_NAME_TITLE + " LIKE ?";
+        String[] selectionArgs = {shoppingItem.getName()};
+        int DeleteRow = db.delete(ShoppingElementEntry.TABLE_NAME, selection, selectionArgs);
+
+    }
+
+
+
+}
+
